4fee08f781f7a100ea4cbdf2a5181085b5a44c6e
andreidorinm <andrei.dorin09@yahoo.com>
2023-12-20 17:55:17 +0200

    data modifications

---
+++ lib/data/educationData.ts
@@ -0,0 +1,16 @@
+const educationData = [
+  {
+    institution: 'Bucharest University of Economic Studies',
+    degree: 'Bachelor in Computer Science',
+    dateRange: '2020 – 2023',
+    location: 'Bucharest, Romania',
+    description: [
+      'Focused on core Computer Science subjects including Data Structures, Algorithms, and Database Management.',
+      'Engaged in projects and coursework covering advanced topics like Machine Learning and Artificial Intelligence.',
+      'Hands-on experience with software development lifecycle, including design, development, testing, and deployment.'
+    ],
+    logoUrl: 'https://csie.ase.ro/wp-content/uploads/2020/10/cropped-CSIE_new-300x132.png', 
+  },
+];
+
+export default educationData;
---
+++ lib/data/projectsData.ts
@@ -0,0 +1,31 @@
+// projectsData.ts
+interface ProjectData {
+  title: string;
+  description: string;
+  url: string;
+  imageUrl: string; // URL for GIF or image
+}
+
+const projectsData: ProjectData[] = [
+  {
+    title: "Vault3",
+    description: "Your safest decentralized vault",
+    url: "https://vault3.live",
+    imageUrl: "https://i.ibb.co/0cg2Lq7/ezgif-com-video-to-gif-converted.gif",
+  },
+  {
+    title: "Vault3",
+    description: "Your safest decentralized vault",
+    url: "https://vault3.live",
+    imageUrl: "https://i.ibb.co/0cg2Lq7/ezgif-com-video-to-gif-converted.gif",
+  },
+  {
+    title: "Vault3",
+    description: "Your safest decentralized vault",
+    url: "https://vault3.live",
+    imageUrl: "https://i.ibb.co/0cg2Lq7/ezgif-com-video-to-gif-converted.gif",
+  },
+  // Add more projects here
+];
+
+export default projectsData;
