a8542c0e10aab9bf6950eebbdd476682526a39b0
andreidorinm <andrei.dorin09@yahoo.com>
2023-12-26 02:15:34 +0200

    removed use reveal for a bit from last section

--- components/Misc/NowPlaying.misc.tsx
+++ components/Misc/NowPlaying.misc.tsx
@@ -3,14 +3,12 @@ import Image from "next/image";
 import Link from "next/link";
 import useSWR from "swr";
 import { fetcher } from "../../lib/fetcher";
-import useRevealAnimation from "../../lib/hooks/useRevealAnimation";
 
 const SpotifyCard = () => {
   const { data } = useSWR<NowPlayingSong>("/api/now-playing", fetcher);
-  const { ref } = useRevealAnimation();
 
   return (
-    <div ref={ref} className="p-4 rounded-lg shadow-lg border border-green-500 mb-3 hidden-component">
+    <div className="p-4 rounded-lg shadow-lg border border-green-500 mb-3 hidden-component">
       <div className="font-sen flex flex-row items-center gap-x-2 text-center md:text-lg text-gray-300">
         {data?.isPlaying ? (
           <>
